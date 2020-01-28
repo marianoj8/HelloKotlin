db7501daff2387aa55dfbb856b8b7b2099be220e
Mariano JavaSwing <marianogoncalves8@gmail.com>
2020-01-28 20:39:10 +0100

    06 - Colecoes e iteracaos (arrayOf)

--- src/Main.kt
+++ src/Main.kt
@@ -2,14 +2,30 @@
 
 fun getGreeting(): String = "Hello Kotlin!"
 
-fun seyHelloTo(message: String) = "Hello $message"
-
-fun seyHello() {
-    println(getGreeting())
-}
-
 fun main() {
-    println(getGreeting())
-    seyHello()
-    println(seyHelloTo("Mariano"))
+    val interestingThings = arrayOf("Java", "Kotlin", "Dart", "Others")
+    println(interestingThings.size)
+    println(interestingThings[0])
+    println(interestingThings.get(0))
+    println("Running loop")
+
+    for (i: String in interestingThings) {
+        println(i)
+    }
+
+    println()
+    println("Using foreEach")
+
+    interestingThings.forEach {
+        println(it)
+    }
+    //Renaming "it"
+    interestingThings.forEach { item ->
+        println(item)
+    }
+
+    //Using indexed foreEach
+    interestingThings.forEachIndexed { index, item ->
+        println("[$index] -> $item")
+    }
 }
